ce6ff9e7bdae6b5b0a460ea9f3f129fe771955d1
bejolo018 <bejolo018@gmail.com>
2020-05-21 17:11:47 -0500

    created shoppinglist component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import AppNavbar from './components/AppNavbar'
-
+import ShoppingList from './components/ShoppingList'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 
@@ -8,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <AppNavbar/ >
+      <ShoppingList />
     </div>
   );
 }
