b21ed2f113f4f334d9370d82a6f3d52033ddf517
Benjamin Lopez <47269255+bejolo018@users.noreply.github.com>
2020-07-27 19:50:43 -0500

    completed login functionality, auth bug on "getting items" needs to be fixed

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { 
     Collapse, 
     Navbar, 
@@ -8,9 +8,11 @@ import {
     NavItem, 
     Container 
 } from 'reactstrap';
-
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import RegisterModal from './auth/RegisterModal';
 import Logout from './auth/Logout';
+import LoginModal from './auth/LoginModal';
 
 class AppNavbar extends Component {
     state = {
@@ -23,7 +25,38 @@ class AppNavbar extends Component {
             });
         }
 
+        static propTypes = {
+            auth: PropTypes.object.isRequired
+        }
+
     render() {
+        const { isAuthenticated, user } = this.props.auth;
+
+        const authLinks = (
+            <Fragment>
+                <NavItem>
+                    <span className="navbar-text mr-3">
+                        <strong>{ user ? `Welcome ${user.name} ` : '' }</strong>
+                    </span>
+                </NavItem>
+                <NavItem>
+                    <Logout />
+                </NavItem>
+            </Fragment>
+        )
+        
+        const guestLinks = (
+            <Fragment>
+                <NavItem>
+                        <RegisterModal />
+                </NavItem>
+                <NavItem>
+                        <LoginModal />
+                </NavItem>
+
+            </Fragment>
+        )
+
         return(
             <div>
             <Navbar color="dark" dark expand="sm" className="mb-5">
@@ -32,12 +65,7 @@ class AppNavbar extends Component {
                     <NavbarToggler onClick={this.toggle} />
                     <Collapse isOpen={this.state.isOpen} navbar>
                         <Nav className="ml-auto" navbar>
-                            <NavItem>
-                                    <RegisterModal />
-                            </NavItem>
-                            <NavItem>
-                                <Logout />
-                            </NavItem>
+                            { isAuthenticated ? authLinks : guestLinks }
                         </Nav>
                     </Collapse>
                 </Container>
@@ -48,5 +76,9 @@ class AppNavbar extends Component {
     }
 
 
+const mapStateToProps = state => ({
+    auth: state.auth
+})
 
-export default AppNavbar;
+
+export default connect(mapStateToProps, null)(AppNavbar);
--- client/src/components/MovieList.js
+++ client/src/components/MovieList.js
@@ -6,6 +6,12 @@ import { getItems, deleteItem } from '../actions/itemActions';
 import PropTypes from 'prop-types';
 
 class MovieList extends Component{
+    static propTypes = {
+        getItems: PropTypes.func.isRequired,
+        item: PropTypes.object.isRequired,
+        isAuthenticated: PropTypes.bool
+    }
+
     componentDidMount(){
         this.props.getItems();
     }
@@ -22,12 +28,12 @@ class MovieList extends Component{
                             {items.map(({ _id, name}) => (
                                 <CSSTransition key={_id} timeout={500} classNames="fade">
                                     <ListGroupItem>
-                                    <Button
+                                    { this.props.isAuthenticated ? <Button
                                         className="remove-btn"
                                         color="danger"
                                         size="sm"
                                         onClick={this.onDeleteClick.bind(this, _id)}
-                                    >&times;</Button>
+                                    >&times;</Button> : null}
                                         {name}
                                     </ListGroupItem>
                                 </CSSTransition>
@@ -39,13 +45,9 @@ class MovieList extends Component{
     }
 }
 
-MovieList.propTypes = {
-    getItems: PropTypes.func.isRequired,
-    item: PropTypes.object.isRequired
-}
-
 const mapStateToProps = (state) =>({
-    item: state.item
+    item: state.item,
+    isAuthenticated: state.auth.isAuthenticated
 })
 
 export default connect(mapStateToProps, { getItems, deleteItem })(MovieList);
