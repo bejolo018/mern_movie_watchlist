c0e36b4b6309cfe44ae86aa47ad16de8e5dfddf0
Benjamin Lopez <47269255+bejolo018@users.noreply.github.com>
2020-07-30 17:32:37 -0500

    fixed bugs, maybe, not very secure yet

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI: 'mongoURI": "mongodb+srv://ben:rudy0619@cluster0-wwvpu.mongodb.net/test?retryWrites=true&w=majority'
+    mongoURI: "mongodb+srv://ben:rudy0619@cluster0-wwvpu.mongodb.net/test?retryWrites=true&w=majority"
 }
